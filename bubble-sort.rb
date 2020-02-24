04ea73c631121612cbafee9dfbef105018226369
Tirthajyoti Ghosh <itirthahere@gmail.com>
2020-02-24 15:20:36 +0530

    completed bubble_sort_by method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -14,11 +14,32 @@ def bubble_sort(array)
 end
 
 #bubble sort with yield
-def bubble_sort_by
+def bubble_sort_by(arr)
+    for x in 1..arr.length-1
+        for y in 0..arr.length-x
+            if y != arr.length-1
+                sort_param = yield(arr[y], arr[y+1])
+
+                if sort_param > 0
+                    arr[y], arr[y+1] = arr[y+1], arr[y]
+                end
+            end
+        end
+    end
+    arr
 end
 
 print bubble_sort([5,4,3,2,1])
 puts
 print bubble_sort(["tirtha","kyle","heh","haha","ruby","microverse"])
+puts
+
+print bubble_sort_by(["tirtha","kyle","heh","haha","ruby","microverse"]) { |first, second|
+    first.length - second.length
+}
+puts
+print bubble_sort_by([4,3,78,2,0,2]) { |first, second| first <=> second }
+
+
 
 
